8211e0fbca250c553c947fcaaaf657131e564482
Sean Youngberg <syoungberg@paloaltonetworks.com>
2021-02-18 12:28:29 -0500

    add separate tfvars and module calls to build spoke app vpcs

--- terraform/vmseries/main.tf
+++ terraform/vmseries/main.tf
@@ -1,3 +1,5 @@
+### Module calls for Security VPC and base infrastructure
+
 module "security_vpc" {
   source           = "../modules/vpc"
   global_tags      = var.global_tags
@@ -12,7 +14,7 @@ module "security_vpc" {
 
 resource "aws_key_pair" "lab" {
   key_name   = var.ssh_key_name
-  public_key = var.public_key_path
+  public_key = file(var.public_key_path)
 }
 
 module "vmseries" {
@@ -75,4 +77,156 @@ module "transit_gateways" {
   transit_gateways                = var.transit_gateways
   transit_gateway_vpc_attachments = var.transit_gateway_vpc_attachments
   transit_gateway_peerings        = var.transit_gateway_peerings
+}
+
+
+### Module calls for App1 VPC
+
+
+
+module "app1_vpc" {
+  source           = "../modules/vpc"
+  global_tags      = var.global_tags
+  prefix_name_tag  = var.prefix_name_tag
+  vpc              = var.app1_vpc
+  vpc_route_tables = var.app1_vpc_route_tables
+  subnets          = var.app1_vpc_subnets
+  vpc_endpoints    = var.app1_vpc_endpoints
+  security_groups  = var.app1_vpc_security_groups
+}
+
+
+module "app1_vpc_routes" {
+  source            = "../modules/vpc_routes"
+  region            = var.region
+  global_tags       = var.global_tags
+  prefix_name_tag   = var.prefix_name_tag
+  vpc_routes        = var.app1_vpc_routes
+  vpc_route_tables  = module.app1_vpc.route_table_ids
+  internet_gateways = module.app1_vpc.internet_gateway_id
+  nat_gateways      = module.app1_vpc.nat_gateway_ids
+  vpc_endpoints     = module.app1_gwlb.endpoint_ids
+  transit_gateways  = module.app1_transit_gateways.transit_gateway_ids
+}
+
+module "app1_transit_gateways" {
+  source                          = "../modules/transit_gateway"
+  global_tags                     = var.global_tags
+  prefix_name_tag                 = var.prefix_name_tag
+  subnets                         = module.app1_vpc.subnet_ids
+  vpcs                            = module.app1_vpc.vpc_id
+  transit_gateways                = var.app1_transit_gateways
+  transit_gateway_vpc_attachments = var.app1_transit_gateway_vpc_attachments
+  depends_on = [module.gwlb] // Depends on GWLB being created in security VPC
+}
+
+module "app1_gwlb" {
+  source                          = "../modules/gwlb"
+  region                          = var.region
+  global_tags                     = var.global_tags
+  prefix_name_tag                 = var.prefix_name_tag
+  vpc_id                          = module.app1_vpc.vpc_id.vpc_id
+  gateway_load_balancers          = var.app1_gateway_load_balancers
+  gateway_load_balancer_endpoints = var.app1_gateway_load_balancer_endpoints
+  subnets_map                     = module.app1_vpc.subnet_ids
+  depends_on = [module.transit_gateways] // Depends on GWLB being created in security VPC
+}
+
+# output "testing" {
+#   value = module.gwlb.endpoints
+# }
+
+
+module "app1_ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+
+  name                   = "app1-cluster"
+  instance_count         = 2
+  associate_public_ip_address = false
+
+  ami                    = "ami-0e999cbd62129e3b1" //TODO lookup ami per region
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.lab.key_name
+  monitoring             = true
+  vpc_security_group_ids = [module.app1_vpc.security_group_ids["web-server-sg"]]
+  subnet_id              = module.app1_vpc.subnet_ids["web1"]
+
+  tags = var.global_tags
+}
+
+
+### Module calls for app2 VPC
+
+
+
+module "app2_vpc" {
+  source           = "../modules/vpc"
+  global_tags      = var.global_tags
+  prefix_name_tag  = var.prefix_name_tag
+  vpc              = var.app2_vpc
+  vpc_route_tables = var.app2_vpc_route_tables
+  subnets          = var.app2_vpc_subnets
+  vpc_endpoints    = var.app2_vpc_endpoints
+  security_groups  = var.app2_vpc_security_groups
+}
+
+
+module "app2_vpc_routes" {
+  source            = "../modules/vpc_routes"
+  region            = var.region
+  global_tags       = var.global_tags
+  prefix_name_tag   = var.prefix_name_tag
+  vpc_routes        = var.app2_vpc_routes
+  vpc_route_tables  = module.app2_vpc.route_table_ids
+  internet_gateways = module.app2_vpc.internet_gateway_id
+  nat_gateways      = module.app2_vpc.nat_gateway_ids
+  vpc_endpoints     = module.app2_gwlb.endpoint_ids
+  transit_gateways  = module.app2_transit_gateways.transit_gateway_ids
+}
+
+module "app2_transit_gateways" {
+  source                          = "../modules/transit_gateway"
+  global_tags                     = var.global_tags
+  prefix_name_tag                 = var.prefix_name_tag
+  subnets                         = module.app2_vpc.subnet_ids
+  vpcs                            = module.app2_vpc.vpc_id
+  transit_gateways                = var.app2_transit_gateways
+  transit_gateway_vpc_attachments = var.app2_transit_gateway_vpc_attachments
+  depends_on = [module.gwlb] // Depends on GWLB being created in security VPC
+}
+
+module "app2_gwlb" {
+  source                          = "../modules/gwlb"
+  region                          = var.region
+  global_tags                     = var.global_tags
+  prefix_name_tag                 = var.prefix_name_tag
+  vpc_id                          = module.app2_vpc.vpc_id.vpc_id
+  gateway_load_balancers          = var.app2_gateway_load_balancers
+  gateway_load_balancer_endpoints = var.app2_gateway_load_balancer_endpoints
+  subnets_map                     = module.app2_vpc.subnet_ids
+  depends_on = [module.transit_gateways] // Depends on GWLB being created in security VPC
+}
+
+# output "testing" {
+#   value = module.gwlb.endpoints
+# }
+
+
+module "app2_ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+
+  name                   = "app2-cluster"
+  instance_count         = 2
+  associate_public_ip_address = false
+
+  ami                    = "ami-0e999cbd62129e3b1" //TODO lookup ami per region
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.lab.key_name
+  monitoring             = true
+  vpc_security_group_ids = [module.app2_vpc.security_group_ids["web-server-sg"]]
+  subnet_id              = module.app2_vpc.subnet_ids["web1"]
+
+  tags = var.global_tags
 }
--- terraform/vmseries/variables.tf
+++ terraform/vmseries/variables.tf
@@ -1,42 +1,63 @@
 ### Global
-variable "region" {}
-variable "prefix_name_tag" {}
-variable "global_tags" {}
-variable "fw_instance_type" {}
-variable "fw_license_type" {}
-variable "fw_version" {}
-variable "ssh_key_name" {}
-variable "public_key_path" {}
+variable region {}
+variable prefix_name_tag {}
+variable global_tags {}
+variable fw_instance_type {}
+variable fw_license_type {}
+variable fw_version {}
+variable ssh_key_name {}
+variable public_key_path {}
 
 ### VPC
-variable "security_vpc" {}
-variable "security_vpc_route_tables" {}
-variable "security_vpc_subnets" {}
-variable "security_vpc_security_groups" {}
-variable "security_nat_gateways" {}
-variable "security_vpc_endpoints" {}
+variable security_vpc {}
+variable security_vpc_route_tables {}
+variable security_vpc_subnets {}
+variable security_vpc_security_groups {}
+variable security_nat_gateways {}
+variable security_vpc_endpoints {}
 
 # VMSERIES
-variable "interfaces" {}
-variable "firewalls" {}
-# variable "addtional_interfaces" {}
+variable interfaces {}
+variable firewalls {}
+# variable addtional_interfaces {}
 
 # VPC_ROUTES
-variable "vpc_routes" {}
+variable vpc_routes {}
 
 # GWLB
-variable "gateway_load_balancers" {}
-variable "gateway_load_balancer_endpoints" {}
-variable "gateway_load_balancer_subnets" {}
+variable gateway_load_balancers {}
+variable gateway_load_balancer_endpoints {}
+variable gateway_load_balancer_subnets {}
+variable transit_gateways { default = {} }
+variable transit_gateway_vpc_attachments { default = {} }
+variable transit_gateway_peerings { default = {} }
 
-variable transit_gateways {
-  default = {}
-}
+### APP1 Variables
 
-variable transit_gateway_vpc_attachments {
-  default = {}
-}
+variable app1_vpc { default = {} }
+variable app1_vpc_route_tables { default = {} }
+variable app1_vpc_subnets { default = {} }
+variable app1_vpc_security_groups { default = {} }
+variable app1_nat_gateways { default = {} }
+variable app1_vpc_endpoints { default = {} }
+variable app1_vpc_routes { default = {} }
+variable app1_gateway_load_balancers { default = {} }
+variable app1_gateway_load_balancer_endpoints { default = {} }
+variable app1_transit_gateways { default = {} }
+variable app1_transit_gateway_vpc_attachments { default = {} }
+variable app1_transit_gateway_peerings { default = {} }
 
-variable transit_gateway_peerings {
-  default = {}
-}
+### APP2 Variables
+
+variable app2_vpc { default = {} }
+variable app2_vpc_route_tables { default = {} }
+variable app2_vpc_subnets { default = {} }
+variable app2_vpc_security_groups { default = {} }
+variable app2_nat_gateways { default = {} }
+variable app2_vpc_endpoints { default = {} }
+variable app2_vpc_routes { default = {} }
+variable app2_gateway_load_balancers { default = {} }
+variable app2_gateway_load_balancer_endpoints { default = {} }
+variable app2_transit_gateways { default = {} }
+variable app2_transit_gateway_vpc_attachments { default = {} }
+variable app2_transit_gateway_peerings { default = {} }
