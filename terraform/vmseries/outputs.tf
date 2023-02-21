e9fe5fc595d3fb17b557073d5c6e2b0d8fb63191
Sean Youngberg <sean.youngberg@gmail.com>
2023-02-21 12:58:24 -0500

    standardizing naming

--- terraform/vmseries/outputs.tf
+++ terraform/vmseries/outputs.tf
@@ -3,12 +3,12 @@ output vmseries_eips {
 }
 
 output endpoint_ids {
-    value = merge(module.gwlb.endpoint_ids, module.app1_gwlb.endpoint_ids, module.app2_gwlb.endpoint_ids)
+    value = merge(module.gwlb.endpoint_ids, module.spoke1_gwlb.endpoint_ids, module.spoke2_gwlb.endpoint_ids)
 }
 
 output app_nlbs_dns {
   value = {
-    app1_nlb = module.app1_nlb.lb_dns_name
-    app2_nlb = module.app2_nlb.lb_dns_name
+    spoke1_nlb = module.spoke1_nlb.lb_dns_name
+    spoke2_nlb = module.spoke2_nlb.lb_dns_name
   }
 }
